e222bb6fd494c9033f13b0f67e399181eac420f0
tekelia-powe <66314860+tekelia-powe@users.noreply.github.com>
2021-09-28 00:07:25 -0500

    Week4 Project

---
+++ Week4/hashing/hashing.py
@@ -0,0 +1,9 @@
+# initializing objects
+int_val = 4
+str_val = 'PythonIsBest'
+flt_val = 24.56
+
+# Printing the hash values.
+print("The integer hash value is : " + str(hash(int_val)))
+print("The string hash value is : " + str(hash(str_val)))
+print("The float hash value is : " + str(hash(flt_val)))
